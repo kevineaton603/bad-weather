753897356490db09ee6f3e2790ca743756210bd3
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2020-04-06 16:00:58 -0400

    creates executable for main file

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#include <httplib.h>
+// #include <httplib.h>
 
 int main () {
-    using namespace httplib;
+  /*  using namespace httplib;
 
     Server svr;
     svr.Get("/hi", [](const Request& req, Response& res) {
     res.set_content("Hello World!", "text/plain");
   });
 
-  svr.listen("localhost", 3000);
+  svr.listen("localhost", 3000);*/
 };
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Bad Weather
+
+## Set-Up
+
+This project was set up on windows using vscode and cmake.
+
+0. Clone the repository
+1. In the root directory, git clone `googletest` and `cpp-httplib`
