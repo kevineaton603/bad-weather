ee186a29e750140718be9933a0dfea2e35790842
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2020-04-08 14:36:09 -0400

    updated app

--- app/app.cpp
+++ app/app.cpp
@@ -3,9 +3,21 @@
 #include <iostream>
 #include <Request.h>
 
+using namespace std;
 
 int main () {
     WeatherRequest req = WeatherRequest();
-    req.getWeather();
+    int lon = 0, lat = 0;
+    cout << "-------------------------------" << endl;
+    cout << "| Welcome to Our Weather App! |" << endl;
+    cout << "-------------------------------" << endl;
+    cout << "Please Enter Your Coordinates" << endl;
+    cout << "Longitude: ";
+    cin >> lon;
+    cout << "Latitude: ";
+    cin >> lat;
+    
+    auto weather = req.getWeather(lon, lat);
+    cout << weather["weather"] << endl;
     return 0;
 }
